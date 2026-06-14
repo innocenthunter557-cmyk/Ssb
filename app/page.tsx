5e1fab4788abb464dead4372702c2b86e35796d3
innocenthunter557-cmyk <innocenthunter557@gmail.com>
2026-06-14 22:04:23 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import LuxuryHero from "@/components/LuxuryHero";
+import AboutLuxury from "@/components/AboutLuxury";
+import FeaturedProducts from "@/components/FeaturedProducts";
+import Testimonials from "@/components/Testimonials";
+import InstagramGallery from "@/components/InstagramGallery";
+
+export default function Home() {
+  return (
+    <>
+      <LuxuryHero />
+      <AboutLuxury />
+      <FeaturedProducts />
+      <Testimonials />
+      <InstagramGallery />
+    </>
+  );
+}
