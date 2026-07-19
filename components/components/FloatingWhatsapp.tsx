426799641ba0593bbdd700813c0425e0cf90f488
innocenthunter557-cmyk <innocenthunter557@gmail.com>
2026-07-19 19:07:59 +0530

    Delete components/components directory

--- components/components/FloatingWhatsapp.tsx
+++
@@ -1,3 +0,0 @@
-export default function FloatingWhatsapp() {
-  return <div style={{ position: 'fixed', bottom: '20px', right: '20px' }}>WhatsApp Button</div>;
-}
--- components/components/Footer.tsx
+++
@@ -1,3 +0,0 @@
-export default function Footer() {
-  return <footer style={{ padding: '1rem', marginTop: '1rem', borderTop: '1px solid #ccc' }}>Footer</footer>;
-}
